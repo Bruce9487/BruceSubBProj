4339392f211728be727520c1e5456c222aeddd4f
APP技術部-陳冠志 <bruce@mitake.com.tw>
2020-07-22 11:39:05 +0800

    Test SubB

---
+++ BruceSubBProj/SubBProjViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SubBProjViewController.swift
+//  BruceSubBProj
+//
+//  Created by APP技術部-陳冠志 on 2020/7/22.
+//  Copyright © 2020 Bruce Chen. All rights reserved.
+//
+
+import UIKit
+
+class SubBProjViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Test SubB")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
